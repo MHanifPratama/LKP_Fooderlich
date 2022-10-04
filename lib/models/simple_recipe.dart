65b580ce072fe93658a1465a5e6fb74adf733fec
MHanifPratama <mhanifp27@gmail.com>
2022-10-04 09:04:47 +0700

    Membuat Model
    Belum Fix

---
+++ lib/models/simple_recipe.dart
@@ -0,0 +1,18 @@
+class SimpleRecipe {
+  String dishImage;
+  String title;
+  String duration;
+  String source;
+  List<String> information;
+
+  SimpleRecipe(
+      required this.dishImage, required this.title, required this.duration, required this.source, required this.information);
+
+  SimpleRecipe.fromJson(Map<String, dynamic> json) {
+    dishImage = json['dishImage'];
+    title = json['title'];
+    duration = json['duration'];
+    source = json['source'];
+    information = json['information'].cast<String>();
+  }
+}
